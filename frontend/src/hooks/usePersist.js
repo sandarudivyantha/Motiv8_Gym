2313c0abdde87af8eb89fafc1ca23532b939a79b
Sandaru Divyantha <79299187+sandarudivyantha@users.noreply.github.com>
2025-03-14 17:32:27 +0530

    usePersist.js, useTitle.js hooks created

--- frontend/src/hooks/usePersist.js
+++ frontend/src/hooks/usePersist.js
@@ -1,3 +1,19 @@
+// // src/hooks/usePersist.js
+// import { useState, useEffect } from 'react'
+
+// const usePersist = () => {
+//   const [persist, setPersist] = useState(JSON.parse(localStorage.getItem('persist')) || false)
+
+//   useEffect(() => {
+//     localStorage.setItem('persist', JSON.stringify(persist))
+//   }, [persist])
+
+//   return [persist, setPersist]
+// }
+
+// export default usePersist
+
+
 // src/hooks/usePersist.js
 import { useState, useEffect } from 'react'
 
